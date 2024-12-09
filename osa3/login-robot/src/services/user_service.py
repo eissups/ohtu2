967aa3c948f43af17b4f3464cdc2cba275b59ef5
elisa <elisa.autonen@gmail.com>
2024-12-09 22:12:36 +0200

    mock demo

--- osa3/login-robot/src/services/user_service.py
+++ osa3/login-robot/src/services/user_service.py
@@ -1,4 +1,5 @@
 from entities.user import User
+import re
 
 
 class UserInputError(Exception):
@@ -32,9 +33,15 @@ class UserService:
         )
 
         return user
+ 
 
     def validate(self, username, password):
         if not username or not password:
             raise UserInputError("Username and password are required")
-
-        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if not re.match("^[a-z]+$", username):  
+            raise UserInputError("Username must only contain letters a-z")
+        if len(username) < 3:
+            raise UserInputError("Username is too short")
+        
+        
+        
---
+++ osa3/login-robot/src/tests/register.robot
@@ -0,0 +1,5 @@
+*** Test Cases ***
+Register With Valid Username And Password
+    Input Credentials  kille  kille123
+    Output Should Contain  Logged in
+
