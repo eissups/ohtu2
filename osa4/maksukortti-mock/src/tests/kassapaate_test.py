42fb49236a77d176af6f97837a4ebb34c4b68040
elisa <elisa.autonen@gmail.com>
2024-12-09 22:47:20 +0200

    kassapaate class + tests

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,21 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    #Kassapäätteen metodin lataa kutsu lisää maksukortille ladattavan rahamäärän käyttäen kortin metodia lataa jos ladattava summa on positiivinen
+    def test_ladataan_rahamaara_jos_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called()
+
+    #Kassapäätteen metodin lataa kutsu ei tee maksukortille mitään jos ladattava summa on negatiivinen
+    def test_ei_ladata_rahamaaraa_jos_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
